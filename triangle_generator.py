cd4ecc014a265672d3101517ab56ebf8b2b48966
Tobias Huber <tobias.huber98@gmx.de>
2022-08-03 23:32:53 +0200

    color list not needed if input provided

--- triangle_generator.py
+++ triangle_generator.py
@@ -30,7 +30,8 @@ output_type = args.out
 outline = args.print_outline
 input = args.input
 color_list = [(ImageColor.getcolor(a, "RGB"), ImageColor.getcolor(b, "RGB")) for a in args.color_list for b in args.color_list if a != b]
-
+if input:
+    color_list = ["", ""]
 print(f"Printing {len(color_list)} pictures")
 count = 0
 for tuple in color_list:
